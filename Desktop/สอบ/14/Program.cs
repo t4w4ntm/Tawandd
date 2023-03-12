b8ed050144e623a61de7e6767ff9520ae3e7c8be
t4w4ntm <122960758+t4w4ntm@users.noreply.github.com>
2023-03-12 17:43:55 +0700

    Add files via upload

---
+++ Desktop/สอบ/14/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program {
+    static void Main(string[] args) {
+            Console.Write("ชั้น =  ");
+            int n = int.Parse(Console.ReadLine());
+        
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n - row; col++) {
+                Console.Write(" ");}
+            Console.Write("#");
+            
+            for (int col = 1; col < 2 * row - 2; col++) {
+                if (row == n) {
+                    Console.Write("#");}
+                else {
+                    Console.Write(" ");}
+            }
+            if (row != 1) {
+                Console.Write("#");
+            }
+            Console.WriteLine();
+        }
+    }
+}
